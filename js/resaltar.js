4c969c378807933b99ef0583cada5548e5282c3d
Jorge Larriva <90697024+jorgelarriva@users.noreply.github.com>
2026-01-04 20:30:47 +0100

    Update resaltar.js
    Correccion busqueda erronea.

--- js/resaltar.js
+++ js/resaltar.js
@@ -7,27 +7,42 @@ class Resaltador {
     }
 
     resaltarTexto() {
+        const patron = this.termino
+            .replace(/[aá]/gi, '[aá]')
+            .replace(/[eé]/gi, '[eé]')
+            .replace(/[ií]/gi, '[ií]')
+            .replace(/[oó]/gi, '[oó]')
+            .replace(/[uú]/gi, '[uú]');
+        
+        const regex = new RegExp(patron, 'gi');
         const walker = document.createTreeWalker(
             document.body,
-            NodeFilter.SHOW_TEXT
+            NodeFilter.SHOW_TEXT,
+            {
+                acceptNode: (node) => {
+                    //ignora menús, footer...
+                    if (node.parentElement.closest("nav, footer, form, script, header")) {
+                        return NodeFilter.FILTER_REJECT;
+                    }
+                    return NodeFilter.FILTER_ACCEPT;
+                }
+            }
         );
 
         let nodo;
+        const nodosParaCambiar = [];
+
         while (nodo = walker.nextNode()) {
-            const texto = nodo.textContent;
-            const term = this.termino.toLowerCase();
-
-            if (texto.toLowerCase().includes(term)) {
-                const span = document.createElement("span");
-
-                span.innerHTML = texto.replace(
-                    new RegExp(this.termino, "gi"),
-                    m => `<mark>${m}</mark>`
-                );
-
-                nodo.parentNode.replaceChild(span, nodo);
+            if (regex.test(nodo.textContent)) {
+                nodosParaCambiar.push(nodo);
             }
         }
+
+        nodosParaCambiar.forEach(nodo => {
+            const span = document.createElement("span");
+            span.innerHTML = nodo.textContent.replace(regex, m => `<mark>${m}</mark>`);
+            nodo.parentNode.replaceChild(span, nodo);
+        });
     }
 }
 
